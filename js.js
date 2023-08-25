dd68575feaefcf34a45b97ae048d2c80b3f1474d
Scrinia <natroyalie@gmail.com>
2023-08-25 16:55:27 -0400

    Update

--- js.js
+++ js.js
@@ -16,26 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
     function createTimelineItem(itemData) {
       const timelineItem = document.createElement('div');
       timelineItem.classList.add('timeline-item');
-      timelineItem.textContent = itemData.title;
-  
-      // Show hover text on item hover
-      timelineItem.addEventListener('mouseenter', () => {
-        const hoverTextContent = document.getElementById('hover-text-content');
-        hoverTextContent.textContent = 'Read More';
-        const hoverTextContainer = document.getElementById('hover-text-container');
-        hoverTextContainer.style.display = 'block';
-      });
-  
-      // Hide hover text on item leave
-      timelineItem.addEventListener('mouseleave', () => {
-        const hoverTextContainer = document.getElementById('hover-text-container');
-        hoverTextContainer.style.display = 'none';
-      });
+      
+      // Create an image element
+      const imageElement = document.createElement('img');
+      imageElement.src = itemData.image; // Assuming your JSON data has an "image" property
+      imageElement.alt = itemData.title;
+      imageElement.classList.add('timeline-image'); // Add a class for styling
+      timelineItem.appendChild(imageElement); // Append the image to the timeline item
+      
+      // Create a title element
+      const titleElement = document.createElement('div');
+      titleElement.classList.add('title');
+      titleElement.textContent = itemData.title;
+      timelineItem.appendChild(titleElement);
   
       // Show "Read More" pop-up on item click
       timelineItem.addEventListener('click', () => {
         const popupContent = document.getElementById('popup-content');
         popupContent.textContent = itemData.description;
+        const popupImage = document.getElementById('popup-image');
+        popupImage.src = itemData.image; // Load the image
         const readMorePopup = document.getElementById('read-more-popup');
         readMorePopup.style.display = 'block';
       });
